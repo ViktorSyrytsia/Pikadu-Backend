a48702c403e878059210196e5dd2db8ab32cd7d4
Oleh-Viktor <syrytsiaviktor@gmail.com>
2020-09-19 15:14:26 +0300

    add session auth with redis

--- src/index.ts
+++ src/index.ts
@@ -3,22 +3,47 @@ import { MikroORM } from '@mikro-orm/core';
 import express from 'express';
 import { ApolloServer } from 'apollo-server-express';
 import { buildSchema } from 'type-graphql';
+import redis from 'redis';
+import session from 'express-session';
+import conncetRedis from 'connect-redis';
 
 import config from './mikro-orm.config';
 import { PostResolver } from './resolvers/post';
 import { UserResolver } from './resolvers/user';
+import { __prod__ } from './constants';
+import { MyContext } from './types';
 
 const main = async () => {
     const orm = await MikroORM.init(config);
     await orm.getMigrator().up();
 
     const app = express();
+
+    const RedisStore = conncetRedis(session);
+    const redisClient = redis.createClient();
+
+    app.use(
+        session({
+            name: 'qid',
+            store: new RedisStore({ client: redisClient, disableTouch: true }),
+            secret: 'secret',
+            saveUninitialized: false,
+            resave: false,
+            cookie: {
+                maxAge: 1000 * 60 * 60 * 24 * 365 * 10,
+                httpOnly: true,
+                sameSite: 'lax',
+                secure: __prod__,
+            },
+        })
+    );
+
     const apolloServer = new ApolloServer({
         schema: await buildSchema({
             resolvers: [PostResolver, UserResolver],
             validate: false,
         }),
-        context: () => ({ em: orm.em }),
+        context: ({ req, res }): MyContext => ({ em: orm.em, req, res }),
     });
 
     apolloServer.applyMiddleware({ app });
--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -1,28 +1,133 @@
-import { User } from 'src/entities/User';
-import { MyContext } from 'src/types';
-import { Resolver, Mutation, InputType, Field, Arg, Ctx } from 'type-graphql';
+import {
+    Resolver,
+    Mutation,
+    InputType,
+    Field,
+    Arg,
+    Ctx,
+    ObjectType,
+    Query,
+} from 'type-graphql';
 import argon2 from 'argon2';
+
+import { User } from '../entities/User';
+import { MyContext } from 'src/types';
 
 @InputType()
 class UsernamePasswordInput {
-    @Field()
+    @Field(() => String)
     username: string;
-    @Field()
+    @Field(() => String)
     password: string;
+}
+
+@ObjectType()
+class FieldError {
+    @Field()
+    field: string;
+    @Field()
+    message: string;
+}
+
+@ObjectType()
+class UserResponse {
+    @Field(() => [FieldError], { nullable: true })
+    errors?: FieldError[];
+
+    @Field(() => User, { nullable: true })
+    user?: User;
 }
 
 @Resolver()
 export class UserResolver {
-    @Mutation()
+    @Query(() => User, { nullable: true })
+    async me(@Ctx() { em, req }: MyContext) {
+        if (!req.session.userId) {
+            return null;
+        }
+        const user = await em.findOne(User, { id: req.session.userId });
+        return user;
+    }
+
+    @Mutation(() => UserResponse)
     async signUp(
         @Arg('options') options: UsernamePasswordInput,
         @Ctx() { em }: MyContext
-    ) {
+    ): Promise<UserResponse> {
+        if (options.username.length <= 2) {
+            return {
+                errors: [
+                    {
+                        field: 'username',
+                        message: 'Invalid username',
+                    },
+                ],
+            };
+        }
+        if (options.password.length <= 5) {
+            return {
+                errors: [
+                    {
+                        field: 'username',
+                        message: 'Invalid password',
+                    },
+                ],
+            };
+        }
+        const user = em.findOne(User, { username: options.username });
+        if (user) {
+            return {
+                errors: [
+                    {
+                        field: 'username',
+                        message: 'User with this username already exist',
+                    },
+                ],
+            };
+        }
         const hashPassword = await argon2.hash(options.password);
-        const user = em.create(User, {
-            username: options.username,
+        const newUser = em.create(User, {
+            username: options.username.toLowerCase(),
             password: hashPassword,
         });
         await em.persistAndFlush(user);
+        return {
+            user: newUser,
+        };
+    }
+
+    @Mutation(() => UserResponse)
+    async signIn(
+        @Arg('options') options: UsernamePasswordInput,
+        @Ctx() { em, req }: MyContext
+    ): Promise<UserResponse> {
+        const user = await em.findOne(User, {
+            username: options.username.toLowerCase(),
+        });
+        if (!user) {
+            return {
+                errors: [
+                    {
+                        field: 'username',
+                        message: "This username doesn't exist",
+                    },
+                ],
+            };
+        }
+        const valid = await argon2.verify(user.password, options.password);
+        if (!valid) {
+            return {
+                errors: [
+                    {
+                        field: 'password',
+                        message: 'Wrong password',
+                    },
+                ],
+            };
+        }
+        req.session.userId = user.id;
+        return {
+            user,
+        };
     }
 }
